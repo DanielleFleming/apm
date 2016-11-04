641bee3349c1e7e80a86e8cb73b5a7162a7e3611
DanielleFleming <danielle@dreamfactoryagency.com>
2016-11-04 12:51:22 -0400

    Add HotJar Tracking Code to header.php

--- header.php
+++ header.php
@@ -18,6 +18,18 @@
 		<?php wp_head(); ?>
 
         <?php echo get_field( 'code-header', 'options' ); ?>
+        <!-- Hotjar Tracking Code for http://www.apm4parts.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:327529,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
     </head>
     <body <?php body_class(); ?>>
 		<?php get_template_part( 'parts/header', 'navigation' ); ?>
