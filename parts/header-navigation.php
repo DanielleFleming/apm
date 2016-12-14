fd9742f5458e34ae08feea2a4afe72d8f66f7af8
cadie <cadie@dreamfactoryagency.com>
2016-12-14 14:27:31 -0500

    Add T3 Pages to Navigation Menu

--- parts/header-navigation.php
+++ parts/header-navigation.php
@@ -23,7 +23,7 @@
                             'menu_class'     => 'nav navbar-nav',
                             'items_wrap'     => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>',
                             'walker'         => new APM_Nav_Walker_Accessibility_Ranger(),
-                            'depth'          => 2
+                            'depth'          => 3
                         )
                     );
                 ?>
