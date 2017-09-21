43339bc2ac42c8fd74dcb23fc79a73fb60e8b16d
cadieb <cadie.brown@gmail.com>
2017-09-21 15:51:01 -0400

    remove dropdown ordering bar from archive-product pages

--- functions.php
+++ functions.php
@@ -477,6 +477,7 @@
 
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
     remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+    remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 
     function woo_custom_product_tab( $tabs ) {
 
